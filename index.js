4053b9a3faacb723fec9f5f338f8bf4a33b145bf
mirko prisciano <mirkoprisciano@gmail.com>
2025-03-08 09:37:07 +0100

    scoping variabili

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-/*1 es */
+
+/*
 let name = "Mirko"
 const city = "lecce"
 
 console.log (name,city)
 
-/*2 es */
+
 let age = 5
 console.log (age)
 
 age = 10
 console.log (age)
 
-/*3 es */
 let number = 7
 {let number = 10
     console.log (number)
 }
-console.log (number)
+console.log (number)*/
+
+
+
+let outside = 12
+{let inside = 23
+    console.log (12)
+    console.log (23)
+}
+console.log (12)
+console.log (23)
