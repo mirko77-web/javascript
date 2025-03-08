832eb5d752b342ef7feac150c656a003949d8275
mirko prisciano <mirkoprisciano@gmail.com>
2025-03-08 09:30:17 +0100

    ridifinizione

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-
+/*1 es */
 let name = "Mirko"
 const city = "lecce"
 
 console.log (name,city)
 
-/*secondo es */
+/*2 es */
 let age = 5
 console.log (age)
 
 age = 10
 console.log (age)
+
+/*3 es */
+let number = 7
+{let number = 10
+    console.log (number)
+}
+console.log (number)
